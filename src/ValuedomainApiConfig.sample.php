b786600b80416dec4fdd7b5d961893a0b88a0ddc
orehid <pota.pota+gh@gmail.com>
2020-09-27 00:37:31 +0900

    add config sample

---
+++ src/ValuedomainApiConfig.sample.php
@@ -0,0 +1,15 @@
+<?php
+// https://www.value-domain.com/vdapi/
+define( 'VALUEDOMAIN_APIKEY','xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx');
+
+// ds record
+$ds_records = [
+    "keytag"      => "9999",
+    "alg"         => "13",   // 13: ECDSAP256SHA256
+    "digesttype"  => "2",    //  2:  SHA256
+    "digest"      => "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+];
+
+// $domain_id   = '999999'; // If you know the value, write it down
+$domain = 'example.com';
+
